a4ba535a2c013b8e67b91ca732e3efa9f43b514c
Vedang Manerikar <ved.manerikar@gmail.com>
2016-11-06 16:23:49 +0530

    Bump the Emacs Up version to 2.0.0

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
 (when (version< emacs-version "25")
   (error "Unsupported Emacs Version! Please upgrade to a newer Emacs.  Emacs installation instructions: https://www.gnu.org/software/emacs/download.html"))
 
-(defvar emacs-up--version "1.0.0"
+(defvar emacs-up--version "v2.0.0"
   "The current version of the Emacs Up Starter Kit.")
 
 (defun emacs-up-version ()
