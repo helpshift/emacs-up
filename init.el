1ba8edd7490f273f60a413e12de8276da481a018
Vedang Manerikar <ved.manerikar@gmail.com>
2020-06-28 12:05:15 +0530

    Bump up the version of emacs-up to v3.4.0
    - Update the version of emacs-up
    - Update the README file
    - Update the NEWS file

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
 (when (version< emacs-version "25")
   (error "Unsupported Emacs Version! Please upgrade to a newer Emacs.  Emacs installation instructions: https://www.gnu.org/software/emacs/download.html"))
 
-(defvar emacs-up--version "v3.3.0"
+(defvar emacs-up--version "v3.4.0"
   "The current version of the Emacs Up Starter Kit.")
 
 (defun emacs-up-version ()
